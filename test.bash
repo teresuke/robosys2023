f0a03fb55905f7b0400e3ff4887e1269a1e7d972
teresuke <shunsuke 0127 teresa heiho->
2023-10-26 13:24:45 +0900

    Add a command test

--- test.bash
+++ test.bash
@@ -1,3 +1,18 @@
-#!/bin/bash
+#!/bin/bash -xv
+# SPDX=FileCopyrightText: 2023 Shunsuke Otani
+# SPDX-Licecse-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng (){
+	echo NG at line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus_stdin)
+[ "${out}" = 14 ] || ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+exit $res
+
